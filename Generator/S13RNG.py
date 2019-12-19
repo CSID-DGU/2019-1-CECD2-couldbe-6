7df345ea094d21fa4e54d7460e28f7e9a1d4aa10
Jinho Yoon <37324479+ek2rlstk@users.noreply.github.com>
2019-12-19 12:37:23 +0900

    Update py

--- Generator/S13RNG.py
+++ Generator/S13RNG.py
@@ -4,23 +4,29 @@ import time
 Dmodular = 2147483647    # Default modular
 Dseed = 0    # Default seed
 
+# S13RNG class
 class S13RNG(object):
     def __init__(self, rng_modular = Dmodular, rng_seed = Dseed):
         self.rng_modular = Dmodular
         self.rng_seed = Dseed
 
+# Return Current Seed
     def current_seed(self):
         return self.rng_seed
 
+# Return Current Modular
     def current_modular(self):
         return self.rng_modular
 
+# Set Seed
     def seed(self, new_seed):
         self.rng_seed = new_seed
 
+# Set Modular
     def modular(self, new_modular):
         self.rng_modular = new_modular
 
+# Run Random and Generate Random Number
     def random(self):
         input = str(self.rng_seed)
         sha = hashlib.new('sha1')
